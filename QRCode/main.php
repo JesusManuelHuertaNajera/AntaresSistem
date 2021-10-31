2aaf16a008d395772d6c99462b5944334c2558c7
Jesus Manuel Huerta Najera <61481286+JesusManuelHuertaNajera@users.noreply.github.com>
2021-10-30 22:22:50 -0500

    v02
    qr listo

--- QRCode/main.php
+++ QRCode/main.php
@@ -67,65 +67,84 @@ $intera = $_POST["intera"];
 
 $contact = $_POST["contact"];
 
+$uso = 1;
 //TOMA LA HORA Y FECHA DE LA CIUDAD DE MEXICO
 date_default_timezone_set('America/Mexico_City');    
 $fecha = date('Y-m-d');  
-$pass = 0;
-if($p21=="SI" OR $p22=="SI" OR $p23=="SI" OR $p24=="SI" OR $p25=="SI" OR $p26=="SI"
-OR $p31=="SI" OR $p32=="SI" OR $p33=="SI" OR $p34=="SI" OR $p35=="SI" OR $p36=="SI" OR $p37=="SI"
-OR $intera == "SI" OR $contact == "SI"
-){
-  header("location: ../warning.php");
-//}elseif($olf =="Si"){
-  //header("location: ../warning.php");
-//}
-//elseif($fiebre=="Si"){
-  //header("location: ../warning.php");
-  
-}
-else{
-  $check = "SELECT * FROM estudiantes WHERE MATRICULA ="."'".$matricula."'";
-  $res = $mysql->query($check);
-  $i = 0;
-  $persona = $res-> fetch_all(MYSQLI_ASSOC);
-  foreach($persona as $up){
+
+$revisar = "SELECT * FROM FORM_COVID WHERE FECHA = "."'".$fecha."'" ." AND P1 ="."'".$matricula."'";
+$resp = $mysql->query($revisar);
+$rowcount=mysqli_num_rows($resp);
+
+if($rowcount > 0){
+    $texto = "Error: Solo Puedes Hacer Tu Registro Una Vez Al Dia.";
+    header("location: ../materror.php?text=$texto");
+}else{
+    $check = "SELECT * FROM ESTUDIANTES WHERE MATRICULA ="."'".$matricula."'";
+    $res = $mysql->query($check);
+    $matT=mysqli_num_rows($resp);
+    $i = 0;
+    $Fo_name;
+    $Fo_date = $fecha;
+    foreach($res as $up){
       $i=$i+1;
-  }
-
-  if($i > 0){
-    
-    //INSERTA LOS DATOS A LA BASE DE DATOS
-    $select = "INSERT INTO `FORM_COVID`(`P1`, `P21`, `P22`, `P23`, `P24`,`P25`, `P26`, `P31`, `P32`, `P33`,`P34`, `P35`, `P36`, `P37`, `P4`,`P5`, `FECHA`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
-    $resultado = $mysql->prepare($select);
-    $resultado -> bind_param("sssssssssssssssss",$matricula,
-    $p21,$p22,$p23,$p24,$p25,$p26
-    ,$p31,$p32,$p33,$p34,$p35,$p36,$p37
-    ,$intera,$contact,$fecha);
-    $resultado ->execute();
-    //header("location: index.php");
-
-    
-
-    //--------------------GENERAR QR----------------------------
-    //data to be stored in qr
-    $text = strval($matricula);
-      
-    //file path
-    $file = "./".$matricula.".jpg";
-
-    //other parameters
-    $ecc = 'H';
-    $pixel_size = 20;
-    $frame_size = 5;
-      
-    // Generates QR Code and Save as PNG
-    QRcode::png($text, $file, $ecc, $pixel_size, $frame_size);
-
-    //TERMINA EL PROCESO MANDANDO A LA PANTALLA FINAL
-    header("location: ../endform.php?mat=$matricula");
-  }else{
-    header("location: ../materror.php");
+      $Fo_name=$up['NOMBRE_C'];
     }
-  }
-  
+    echo $i;
+    if($i > 0){
+        if($p21=="SI" or $p22=="SI" or $p23=="SI" or $p24=="SI" or $p25=="SI" or $p31=="SI" or $p32=="SI" or $p33=="SI" or $p34=="SI" or $p35=="SI" or $p36=="SI"             or $intera == "SI" or $contact == "SI"){
+        $p26 = "NO";
+        $p37 = "NO";
+        //INSERTA LOS DATOS A LA BASE DE DATOS
+        $select = "INSERT INTO `FORM_COVID`(`P1`, `P21`, `P22`, `P23`, `P24`,`P25`, `P26`, `P31`, `P32`, `P33`,`P34`, `P35`, `P36`, `P37`, `P4`,`P5`, `FECHA`,                `USO`) VALUES           (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+        $resultado = $mysql->prepare($select);
+        $resultado -> bind_param("sssssssssssssssssi",$matricula,
+        $p21,$p22,$p23,$p24,$p25,$p26
+        ,$p31,$p32,$p33,$p34,$p35,$p36,$p37
+        ,$intera,$contact,$fecha,$uso);
+        $resultado ->execute();
+        
+        header("location: ../warning.php");
+        }else{
+            $p26 = "SI";
+            $p37 = "SI";
+            //INSERTA LOS DATOS A LA BASE DE DATOS
+            $select = "INSERT INTO `FORM_COVID`(`P1`, `P21`, `P22`, `P23`, `P24`,`P25`, `P26`, `P31`, `P32`, `P33`,`P34`, `P35`, `P36`, `P37`, `P4`,`P5`, `FECHA`,                `USO`) VALUES           (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+            $resultado = $mysql->prepare($select);
+            $resultado -> bind_param("sssssssssssssssssi",$matricula,
+            $p21,$p22,$p23,$p24,$p25,$p26
+            ,$p31,$p32,$p33,$p34,$p35,$p36,$p37
+            ,$intera,$contact,$fecha,$uso);
+            $resultado ->execute();
+            //header("location: index.php");
+
+            $content_id;
+            $ObtenerId = "SELECT ID FROM FORM_COVID WHERE FECHA = "."'".$fecha."'" ." AND P1 ="."'".$matricula."'";
+            $info = $mysql->query($ObtenerId);
+            foreach($info as $text){
+              $content_id = $text['ID'];
+            }
+            //--------------------GENERAR QR----------------------------
+            //data to be stored in qr
+            $text = strval($content_id);
+            
+            //file path
+            $file = "./images/".$matricula.".jpg";
+
+            //other parameters
+            $ecc = 'H';
+            $pixel_size = 20;
+            $frame_size = 5;
+            
+            // Generates QR Code and Save as PNG
+            QRcode::png($text, $file, $ecc, $pixel_size, $frame_size);
+
+            //TERMINA EL PROCESO MANDANDO A LA PANTALLA FINAL
+            header("location:../endform.php?mat=$matricula&name=$Fo_name&date=$Fo_date");
+        }
+    }else{
+        $texto = "Error Con La Matricula";
+        header("location: ../materror.php?text=$texto");
+    }
+    }
 ?>
