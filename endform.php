44020955e040c65b614abe088f96083215f4fca8
Jesus Manuel Huerta Najera <61481286+JesusManuelHuertaNajera@users.noreply.github.com>
2021-11-05 22:12:54 -0600

    v05
    correccion de error

--- endform.php
+++ endform.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" type="text/css" href="./librerias/bulma/css/bulma.min.css">
   </head>
   <body>
-      <!-- NAVEGACI�0�7N -->
+      <!-- NAVEGACIÓN -->
       <nav class="navbar is-link " role="navigation" aria-label="main navigation">
         <div class="container">
                 <div class="navbar-brand">
@@ -17,42 +17,57 @@
                   </a> 
                 </div>
                 <div class="navbar-end">
-                    <a href="./form.php" class="navbar-item">
+                 <a href="./form.php" class="navbar-item">
                       Regresar
                     </a>
-<!--
+   <!--
                     <a href="./docker.html" class="navbar-item">
                       Docker
                     </a>-->
                 </div>
         </div>
-    </nav><!-- NAVEGACI�0�7N -->
+    </nav><!-- NAVEGACIÓN -->
   <!-- HERO -->
-        
+      <?php 
+       $matricula = $_GET['mat'];
+       $name = $_GET['name'];
+       $fecha = $_GET['date'];
+      
+       $imagen = "./QRCode/images/".$matricula.".jpg"
+      ?> 
   <div class="has-background-grey-lighter">
     <div class="container my-5 px-3">
         <section class="columns">
            
             <div class="column is-third-desktop is-flex is-flex-direction-column is-justify-content-space-between is-three-quarters-mobile">
                 <div>
-                    <h1 class="is-size-1 has-text-black-bis has-text-weight-bold line-height-3 mb-4">
-                    <center> Acceso denegado. </center>
-                    </h1>
-                </div>
-                <BR>
-                <div>
-                    <h3 class="is-size-4 has-text-black-bis has-text-weight-bold line-height-3 mb-4">
-                    <center> Probable situación de riesgo.</center>
+                    <h3 class="is-size-2 has-text-black-bis has-text-weight-bold line-height-3 ">
+                    <center><?php  echo "Nombre: ".$name ?> </center>
                     </h3>
-                    <h3 class="is-size-4 has-text-black-bis has-text-weight-bold line-height-3 mb-4">
-                    <center> Comunicar la situación al Colegio.</center>
+                    <h3 class="is-size-2 has-text-black-bis has-text-weight-bold line-height-3 ">
+                    <center> <?php  echo "Fecha: ".$fecha ?></center>
                     </h3>
                 </div>
-                <BR>
                 <div>
                     <center>
-                    <img src= "./images/warning.png"  alt="Image" style="width:300px;height:300px;">
+                    <img src= <?php  echo $imagen ?>  alt="Image" style="width:300px;height:300px;">
                     </center>
+                </div>
+                <BR>
+                <div>
+                  <center>
+                  <a class="button is-success is-rounded has-text-black" href="<?php echo "./QRCode/descarga.php?mat=$matricula" ?>">
+                  Descargar
+                  <img src= "./images/R.png"  alt="Image" style="width:40px;height:20px;">
+                </a>
+                  </center>
+                </div>
+                <BR>
+                <div>
+                    <h3 class="is-size-4 has-text-black-bis has-text-weight-bold line-height-3 mb-4">
+                    <center> Has terminado tu proceso, ya puedes cerrar la pestaña.</center>
+                     
+                    </h3>
                 </div>
             </div>
            
