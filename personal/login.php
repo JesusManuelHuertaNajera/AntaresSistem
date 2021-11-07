f5278aaa26ff51c45ccbd7fb2220a2eb52ebfd4e
Jesus Manuel Huerta Najera <61481286+JesusManuelHuertaNajera@users.noreply.github.com>
2021-11-07 16:25:08 -0600

    v02
    error title

--- personal/login.php
+++ personal/login.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Login Form in Bulma</title>
+    <title>Colegio</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
